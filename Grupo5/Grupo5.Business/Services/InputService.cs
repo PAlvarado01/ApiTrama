ed1e90313dc49e51359ff35a4c007bb6e66f61c7
PAlvarado01 <101817855+PAlvarado01@users.noreply.github.com>
2024-07-02 17:18:02 -0500

    limpiado de codigo comentado

--- Grupo5/Grupo5.Business/Services/InputService.cs
+++ Grupo5/Grupo5.Business/Services/InputService.cs
@@ -6,26 +6,7 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace Grupo5.Business.Services
 {
     public abstract class InputService
-        //<TInput, TValidator> where TInput : IInput where TValidator : AbstractValidator<TInput>
     {
-        //public AbstractValidator<TInput> GetValidator() => Activator.CreateInstance<TValidator>();
-
-        //public ValidationResult Validate(IInput input) => GetValidator().Validate((TInput)input);
-
-        //public string SerializeErrors(IInput input)
-        //{
-        //    var builder = new StringBuilder();
-        //    var errors = Validate(input).Errors.ToList();
-
-        //    foreach (var error in errors)
-        //    {
-        //        error.PropertyName = error.PropertyName.Split('.').ToList().LastOrDefault();
-        //        builder.AppendLine($"{error.ErrorMessage} <br/>");
-        //    }
-
-        //    return builder.ToString();
-        //}
-
         protected string Validations<TV, TE>(TV validacao, TE entidade)
             where TV : AbstractValidator<TE>
             where TE : Request
